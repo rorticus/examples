e8d19f9487d8d9b7de6d553d5218d32d01f69c0b
Rory Mulligan <rorticus@gmail.com>
2016-12-14 12:01:12 -0600

    Fixing styles

--- todo-mvc/src/widgets/createTodoCardList.ts
+++ todo-mvc/src/widgets/createTodoCardList.ts
@@ -4,6 +4,7 @@ import { w, v } from 'dojo-widgets/d';
 import createTodoCardItem from './createTodoCardItem';
 import { TodoItemState } from './createTodoListItem';
 import { TodoList } from './createTodoItemList';
+import { VNodeProperties } from 'dojo-interfaces/vdom';
 
 type TodoListState = WidgetState & {
 	activeFilter?: string;
@@ -26,6 +27,17 @@ const createTodoItemList = createWidgetBase.mixin({
 	mixin: {
 		tagName: 'ul',
 		classes: [ 'todo-list', 'card-list' ],
+		nodeAttributes: [
+			function(this: any): VNodeProperties {
+				const { todos = [] } = this.state;
+
+				return {
+					classes: {
+						empty: !todos.length
+					}
+				};
+			}
+		],
 		getChildrenNodes: function (this: TodoList): DNode[] {
 			const activeFilter = this.state.activeFilter || '';
 			const todos = this.state.todos || [];
